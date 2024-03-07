9bf46e88bea54e6cd5aed8faa851f945ab55d360
moraruCristianMarian <morarucristianmarian@yahoo.com>
2024-03-07 17:48:13 +0200

    Display shop elements
    - Pieces in the shop menu are now generated at start according to an editable list in the shop prefab;
    - Shop elements are spaced accordingly and the height of the scrollable shop content area depends on the number of shop elements.

--- GGG/Assets/Scripts/ShopMenuScript.cs
+++ GGG/Assets/Scripts/ShopMenuScript.cs
@@ -7,4 +7,20 @@ public class ShopMenuScript : MonoBehaviour
     public GameObject PiecePrefab;
     public List<int> Prices;
     public List<Sprite> Sprites;
+
+    private void Start()
+    {
+        RectTransform shopContentPanel = GetComponent<RectTransform>();
+        shopContentPanel.sizeDelta = new Vector2(shopContentPanel.sizeDelta.x, Sprites.Count * 100);
+
+        for (int i = 0; i < Sprites.Count; i++)
+        {
+            GameObject shopPiece = Instantiate(PiecePrefab);
+
+            shopPiece.transform.SetParent(gameObject.transform, false);
+            shopPiece.transform.position = new Vector2(0, -(i+1) * 75);
+
+            shopPiece.GetComponent<UnityEngine.UI.Image>().sprite = Sprites[i];
+        }
+    }
 }
